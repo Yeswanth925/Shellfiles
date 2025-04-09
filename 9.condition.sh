e8286650623387e68cb7252091ff8211c6c0cec4
yeswanth <yeshureddy1999@gmail.comgit config --global user.name yeswanthgit pushgit config --global user.email yeshureddy1999@gmail.com>
2025-04-09 22:04:05 +0530

    added

--- 9.condition.sh
+++ 9.condition.sh
@@ -10,6 +10,18 @@ else
     echo " you are root user "
 fi
 
+sudo apt install mysql-server -y
 
 
+if [ $? -ne 0]
+then
+    echo "Installation failed"
+exit 100
+else
+    echo "Installation sucess"
+fi
 
+# 1. If mysql failed to install\
+# exit echo failed
+#2. If mysql is installed
+# echo sucess
