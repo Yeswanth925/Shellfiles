359923babf81a6889d2ce4b77f119ef5291c00c3
yeswanth <yeshureddy1999@gmail.comgit config --global user.name yeswanthgit pushgit config --global user.email yeshureddy1999@gmail.com>
2025-04-09 22:13:26 +0530

    added

---
+++ 10.function.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ID=$( id -u )
+
+    VALIDATE(){
+    if [ $? -ne 0]
+    then
+        echo "Installation failed"
+    exit 100
+    else
+        echo "Installation sucess"
+    fi
+ 
+}
+
+if [ $ID -ne 0 ]
+ then
+    echo " ERROR: Please proceed with root user"
+    exit 1
+else
+    echo " you are root user "
+fi
+
+sudo apt install mysql-server -y
+VALIDATE
+
+sudo apt install git -y
+VALIDATE
