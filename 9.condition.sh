d8d5092411a90ca1148eed71a0d0f4725a161472
yeswanth <yeshureddy1999@gmail.comgit config --global user.name yeswanthgit pushgit config --global user.email yeshureddy1999@gmail.com>
2025-04-09 15:54:43 +0530

    added

--- 9.condition.sh
+++ 9.condition.sh
@@ -1,12 +1,12 @@
-#!/nin/bash
+#!/bin/bash
 
 
 ID=$(id-u)
 
-if ($ID -ne 0)
+if ( $ID -ne 0 )
 then
-echo "ERROR:please proceed with root user"
-EXIT1
+    echo "ERROR:please proceed with root user"
+    EXIT1
 else 
-echo "you are a root user"
+    echo "you are a root user"
 fi
